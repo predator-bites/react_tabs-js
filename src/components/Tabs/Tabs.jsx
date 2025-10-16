e0eaa86d20686c25c99850a3935124e49200f2bc
DropPredator <ionchiccc@gmail.com>
2025-10-16 11:30:36 +0300

    ready steady

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected, activeTab }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={cn({
+              'is-active': activeTabId === tab.id,
+            })}
+            data-cy="Tab"
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => onTabSelected(tab)}
+            >
+              {tab.id}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div className="block" data-cy="TabContent" key={activeTab.id}>
+      {activeTab.content}
+    </div>
+  </div>
+);
